7f502e34569f6721abfce5908459bca041a7d956
ozen-m <74519617+ozen-m@users.noreply.github.com>
2025-12-14 16:53:33 +0800

    Retarget lights patch + Fixes hold tactical devices

--- BatteriesNotIncluded/Patches/Tactical/CanChangeLightStatePatch.cs
+++ BatteriesNotIncluded/Patches/Tactical/CanChangeLightStatePatch.cs
@@ -10,26 +10,21 @@ public class CanChangeLightStatePatch : ModulePatch
 {
     protected override MethodBase GetTargetMethod()
     {
-        return typeof(Player.FirearmController.GClass2013)
-            .GetMethod(nameof(Player.FirearmController.GClass2013.CanChangeLightState));
+        return typeof(Player.FirearmController).GetMethod(nameof(Player.FirearmController.SetLightsState));
     }
-
-    // BUG: Hold tactical devices does not stop draining battery
 
     [PatchPostfix]
     protected static void Postfix(Player.FirearmController.GClass2013 __instance, ref FirearmLightStateStruct[] lightsStates, ref bool __result)
     {
-        if (!__result) return;
-
         var manager = Singleton<DeviceManager>.Instance;
         for (var i = 0; i < lightsStates.Length; i++)
         {
             ref var lightsState = ref lightsStates[i];
-            if (manager.IsItemRegistered(lightsState.Id))
-            {
-                // Light component doesn't have an event we can subscribe to, do it here.
-                manager.RunManualUpdateNextFrame();
-            }
+
+            // Light component doesn't have an event we can subscribe to, do it here.
+            // IsRegistered check?
+            manager.ManualUpdate(lightsState.Id);
+
             if (lightsState.IsActive && !manager.GetIsOperable(lightsState.Id))
             {
                 lightsState.IsActive = false;
