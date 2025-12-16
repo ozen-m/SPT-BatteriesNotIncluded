df40ed1f701ec3c96ee609b28e96fb0ea4587a71
ozen-m <74519617+ozen-m@users.noreply.github.com>
2025-12-17 02:22:32 +0800

    BotLight patch

---
+++ BatteriesNotIncluded/Patches/Tactical/Bot/TurnOnPatch.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using BatteriesNotIncluded.Managers;
+using Comfort.Common;
+using SPT.Reflection.Patching;
+
+namespace BatteriesNotIncluded.Patches.Tactical.Bot;
+
+/// <summary>
+/// Patch BotLight so that the bot would not continously try to turn on devices with drained batteries
+/// </summary>
+public class TurnOnPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(BotLight).GetMethod(nameof(BotLight.TurnOn));
+    }
+
+    [PatchPostfix]
+    protected static void Postfix(BotLight __instance)
+    {
+        if (!__instance.HaveLight) return;
+
+        if (!Singleton<DeviceManager>.Instance.GetIsOperable(__instance.LightMod.Item))
+        {
+            __instance.HaveLight = false;
+        }
+    }
+}
