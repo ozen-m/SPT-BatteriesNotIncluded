1a8bb466fe3e1ab6d6365fa412f61beee486159c
ozen-m <74519617+ozen-m@users.noreply.github.com>
2025-12-28 22:28:20 +0800

    Camera null checks + Fix loading into raid with nvgs

--- BatteriesNotIncluded/Utils/CameraUtil.cs
+++ BatteriesNotIncluded/Utils/CameraUtil.cs
@@ -4,11 +4,21 @@ public static class CameraUtil
 {
     public static void ToggleThermalVision() => CameraClass.Instance.ThermalVision.On = !CameraClass.Instance.ThermalVision.On;
 
-    public static void SetThermalVision(bool state) => CameraClass.Instance.ThermalVision.On = state;
+    public static void SetThermalVision(bool state)
+    {
+        if (CameraClass.Instance.ThermalVision == null) return;
+        
+        CameraClass.Instance.ThermalVision.On = state;
+    }
 
     public static void ToggleNightVision() => CameraClass.Instance.NightVision.On = !CameraClass.Instance.NightVision.On;
 
-    public static void SetNightVision(bool state) => CameraClass.Instance.NightVision.On = state;
+    public static void SetNightVision(bool state)
+    {
+        if (CameraClass.Instance.NightVision == null) return;
+
+        CameraClass.Instance.NightVision.On = state;
+    }
 
     public static bool NightVisionInProcessSwitching => CameraClass.Instance.NightVision.InProcessSwitching;
 
