88f683ba849b03064fb137f60326ba83159b7b7e
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-02-04 22:54:06 +0800

    Update APBS compat (2.1.x)

--- BatteriesNotIncluded.Server/Utils/ApbsCompatibility.cs
+++ BatteriesNotIncluded.Server/Utils/ApbsCompatibility.cs
@@ -2,15 +2,18 @@
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.Models.Common;
 using SPTarkov.Server.Core.Models.Spt.Mod;
+using Version = SemanticVersioning.Version;
 
 namespace BatteriesNotIncluded.Utils;
 
 [Injectable(InjectionType.Singleton)]
 public class ApbsCompatibility(IReadOnlyList<SptMod> sptMods, LoggerUtil loggerUtil, IServiceProvider serviceProvider)
 {
-    private bool? _dataLoaderFound;
-    private Type _dataLoaderType;
-    private object _dataLoader;
+    private readonly Version _minVersion = new(2, 1, 0);
+    private readonly Version _maxVersion = new(2, 1, 999);
+
+    private bool? _getTierModsDataFound;
+    private Func<int, Dictionary<MongoId, Dictionary<string, HashSet<MongoId>>>> _getTierModsData;
 
     public Dictionary<MongoId, Dictionary<string, HashSet<MongoId>>>[] GetModsData(int tierCount)
     {
@@ -19,20 +22,19 @@ public class ApbsCompatibility(IReadOnlyList<SptMod> sptMods, LoggerUtil loggerU
         var modTiers = new Dictionary<MongoId, Dictionary<string, HashSet<MongoId>>>[tierCount];
         for (var i = 0; i < tierCount; i++)
         {
-            var modTierMethod = AccessTools.PropertyGetter(_dataLoaderType, $"Tier{i}ModsData");
-            if (modTierMethod is null)
+            try
             {
-                loggerUtil.Warning($"[APBS Compatibility] Tier{i}ModsData cannot be found");
-                modTiers[i] = null;
-                continue;
+                var modTier = _getTierModsData(i);
+                if (modTier is null)
+                {
+                    loggerUtil.Warning($"[APBS Compatibility] Mods data for tier {i} is null");
+                }
+                modTiers[i] = modTier;
             }
-
-            var modTier = modTierMethod.Invoke(_dataLoader, null) as Dictionary<MongoId, Dictionary<string, HashSet<MongoId>>>;
-            if (modTier is null)
+            catch (Exception)
             {
-                loggerUtil.Warning($"[APBS Compatibility] Tier{i}ModsData returned null");
+                loggerUtil.Warning($"[APBS Compatibility] Mods data for tier {i} is invalid");
             }
-            modTiers[i] = modTier;
         }
 
         return modTiers;
@@ -40,30 +42,46 @@ public class ApbsCompatibility(IReadOnlyList<SptMod> sptMods, LoggerUtil loggerU
 
     private bool Init()
     {
-        if (_dataLoaderFound.HasValue) return _dataLoaderFound.Value;
+        if (_getTierModsDataFound.HasValue) return _getTierModsDataFound.Value;
 
-        _dataLoaderFound = false;
+        _getTierModsDataFound = false;
         var apbsMod = sptMods.FirstOrDefault(m => m.ModMetadata.ModGuid == "com.acidphantasm.progressivebotsystem");
-        if (apbsMod is null) return _dataLoaderFound.Value;
+        if (apbsMod is null) return _getTierModsDataFound.Value;
+
+        if (apbsMod.ModMetadata.Version < _minVersion || apbsMod.ModMetadata.Version > _maxVersion)
+        {
+            loggerUtil.Warning($"[APBS Compatibility] APBS found with incompatible version {apbsMod.ModMetadata.Version}, supported versions {_minVersion} - {_maxVersion}");
+            return _getTierModsDataFound.Value;
+        }
 
         foreach (var assembly in apbsMod.Assemblies)
         {
-            _dataLoaderType = assembly.GetType("_progressiveBotSystem.Helpers.DataLoader");
-            if (_dataLoaderType == null) continue;
+            var botConfigHelperType = assembly.GetType("_progressiveBotSystem.Helpers.BotConfigHelper");
+            if (botConfigHelperType == null) continue;
 
-            _dataLoader = serviceProvider.GetService(_dataLoaderType);
-            if (_dataLoader == null) continue;
+            var botConfigHelper = serviceProvider.GetService(botConfigHelperType);
+            if (botConfigHelper == null) continue;
 
-            loggerUtil.Debug("[APBS Compatibility] DataLoader found");
-            _dataLoaderFound = true;
+            var getTierModsDataMethod = AccessTools.Method(botConfigHelperType, "GetTierModsData");
+            if (getTierModsDataMethod == null) continue;
+
+            _getTierModsData =
+                AccessTools.MethodDelegate<Func<int, Dictionary<MongoId, Dictionary<string, HashSet<MongoId>>>>>(
+                    getTierModsDataMethod,
+                    botConfigHelper
+                );
+            if (_getTierModsData == null) continue;
+
+            _getTierModsDataFound = true;
+            loggerUtil.Debug("[APBS Compatibility] GetTierModsData found");
             break;
         }
 
-        if (!_dataLoaderFound.Value)
+        if (!_getTierModsDataFound.Value)
         {
-            loggerUtil.Warning($"[APBS Compatibility] APBS is installed but cannot find DataLoader, something went wrong");
+            loggerUtil.Warning($"[APBS Compatibility] APBS is installed but cannot find GetTierModsData, something went wrong");
         }
 
-        return _dataLoaderFound.Value;
+        return _getTierModsDataFound.Value;
     }
 }
