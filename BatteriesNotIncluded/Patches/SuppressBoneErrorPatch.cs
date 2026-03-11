3d0c35be30ec226ffda85e9c960aa7a434644241
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-03-12 01:04:27 +0800

    Suppress bone errors

---
+++ BatteriesNotIncluded/Patches/SuppressBoneErrorPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace BatteriesNotIncluded.Patches;
+
+public class SuppressBoneErrorPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return AccessTools.DeclaredMethod(typeof(LoggerClass), nameof(LoggerClass.LogError));
+    }
+
+    [PatchPrefix]
+    public static bool PatchPrefix(string format)
+    {
+        if (format.StartsWith("bone mod_equipment_00", StringComparison.Ordinal))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
