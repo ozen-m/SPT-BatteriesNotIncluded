cd196a9d5c64e6a79b1e948a62e7e09067b9ac93
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-04-25 02:22:22 +0800

    Up v Fika Sync + Recompile for Fika 2.2.4

--- BatteriesNotIncluded.FikaSync/BatteriesNotIncluded.FikaSync.cs
+++ BatteriesNotIncluded.FikaSync/BatteriesNotIncluded.FikaSync.cs
@@ -10,9 +10,9 @@ using SPT.Reflection.Patching;
 
 namespace BatteriesNotIncluded.FikaSync;
 
-[BepInPlugin("com.ozen.batteriesnotincluded.fikasync", "Batteries Not Included Fika Sync", "1.0.3")]
+[BepInPlugin("com.ozen.batteriesnotincluded.fikasync", "Batteries Not Included Fika Sync", "1.0.4")]
 [BepInDependency("com.ozen.batteriesnotincluded", "1.0.4")]
-[BepInDependency("com.fika.core")]
+[BepInDependency("com.fika.core", "2.2.4")]
 public class BatteriesNotIncludedSync : BaseUnityPlugin
 {
     public static ManualLogSource LogSource;
